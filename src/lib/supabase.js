e41d94e19210e60c0d3049252637f640e660fd1e
Paulo Vitor <vitorsantosp480@gmail.com>
2026-04-14 20:51:48 -0300

    pronto?2

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,2 +1,6 @@
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://crfezleomfildocxcxkx.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyZmV6bGVvbWZpbGRvY3hjeGt4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMDA1MDksImV4cCI6MjA5MTc3NjUwOX0.SW-stfrvOiX4eHUAOnxnfQ0U7Gr6HVlWnq57PjRAZUo';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
